4ddef8463ede695ddc035f59563c3bf248e455aa
Rishimithun Muralidharan <rishimithun.muralidaran@inivossl.com>
2023-11-03 14:43:50 +0530

    401 error screen: done

---
+++ lib/accountnotexist.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class AccountNotExist extends StatelessWidget {
+  AccountNotExist({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Center(
+            child: Padding(
+              padding: EdgeInsets.only(top: 150),
+              child: Image.asset(
+                'assets/images/accountnotexist.png',
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 60, 0, 0),
+            child: GestureDetector(
+              onTap: () {
+                // Navigator.of(context).push(MaterialPageRoute(
+                //   builder: AccessDenied(),
+                // ));
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(239, 129, 56, 1.0),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                padding: EdgeInsets.fromLTRB(50, 20, 50, 20),
+                child: Text(
+                  'Return to Login',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
